72fba5b7f71cc031545e658ef1cea812b64fe8b2
mibrahim2009 <mibrahim2009@yahoo.com>
2012-04-03 12:00:09 +0300

    Update launchctrlresponder.hh

--- launchctrlresponder.hh
+++ launchctrlresponder.hh
@@ -20,21 +20,23 @@ class LaunchCtrlResponder : public Element { public:
 	
 	int configure(Vector<String> &, ErrorHandler *);
 
-	//int LaunchCtrlResponder::initialize(ErrorHandler *);
 
 	Packet *simple_action(Packet *);
 
 	private:
 
 	uint32_t _my_lat;			// Sender's Latitude.
-    uint32_t _my_long;			// Sender's Longitude.
+        
+        uint32_t _my_long;			// Sender's Longitude.
 	
+	// PU behavior of the three candidates channels
 	uint32_t _pu_behavior0;
 	uint32_t _pu_behavior1;
 	uint32_t _pu_behavior2;
 	
 	uint32_t switching_time;
 	
+	//The header of Launch control packet
 	struct launch_ctrl_hdr lch;
 };
 CLICK_ENDDECLS
